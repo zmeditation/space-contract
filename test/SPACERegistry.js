dca83d9583cb57e2e3fd930ab3cd0ad81b120794
zmeditation <senior.zsoftware@gmail.com>
2022-01-13 22:25:40 +0900

    Update contract

--- test/SPACERegistry.js
+++ test/SPACERegistry.js
@@ -33,6 +33,8 @@ contract("SPACERegistry", (accounts) => {
   let contracts = null;
   let estate = null;
   let space = null;
+  let proxy = null;
+  let registry = null;
 
   const params = {
     gas: 7e6,
@@ -53,6 +55,8 @@ contract("SPACERegistry", (accounts) => {
     contracts = await setupUnicialContracts(creator, params);
     space = contracts.space;
     estate = contracts.estate;
+    proxy = contracts.proxy;
+    registry = contracts.registry;
 
     console.log("   ================= Before Each ==============");
     console.log("   Proxy Address    : ", proxy.address);
@@ -65,19 +69,19 @@ contract("SPACERegistry", (accounts) => {
     await space.assignNewRood(0, 2, user, sentByCreator);
   });
 
-  //   describe("CHECK TOTAL SUPPLY", function () {
-  //     it("check a total supply equivalent with initial supply", async function () {
-  //       const totalSupply = await space.totalSupply();
-  //       web3.utils.fromWei(totalSupply, "wei").should.be.equal("2");
-  //     });
-  //     it("check a total supply that increases after creating a new SPACE", async function () {
-  //       let totalSupply = await space.totalSupply();
-  //       web3.utils.fromWei(totalSupply, "wei").should.be.equal("2");
-  //       await space.assignNewRood(300, 900, anotherUser, sentByCreator);
-  //       totalSupply = await space.totalSupply();
-  //       web3.utils.fromWei(totalSupply, "wei").should.be.equal("3");
-  //     });
-  //   });
+  describe("CHECK TOTAL SUPPLY", function () {
+    it("check a total supply equivalent with initial supply", async function () {
+      const totalSupply = await space.totalSupply();
+      web3.utils.fromWei(totalSupply, "wei").should.be.equal("2");
+    });
+    it("check a total supply that increases after creating a new SPACE", async function () {
+      let totalSupply = await space.totalSupply();
+      web3.utils.fromWei(totalSupply, "wei").should.be.equal("2");
+      await space.assignNewRood(300, 900, anotherUser, sentByCreator);
+      totalSupply = await space.totalSupply();
+      web3.utils.fromWei(totalSupply, "wei").should.be.equal("3");
+    });
+  });
 
   //   describe("CHECK SYMBOL", function () {
   //     it("check a space symbol", async function () {
@@ -169,52 +173,52 @@ contract("SPACERegistry", (accounts) => {
   //   });
   // });
 
-  describe("TRANSFER SPACE TO ESTATE", function () {
-    let estateId;
+  // describe("TRANSFER SPACE TO ESTATE", function () {
+  //   let estateId;
 
-    beforeEach(async function () {
-      const estateAddr = await space.estateRegistry();
-      console.log("   Real Estate Address: ", estate.address);
-      console.log("   Estate Address     : ", estateAddr);
-      await space.assignMultipleRoods([3], [3], creator, sentByCreator);
-      estateId = await createEstate([3], [3], user, sentByCreator);
+  //   beforeEach(async function () {
+  //     const estateAddr = await space.estateRegistry();
+  //     console.log("   Real Estate Address: ", estate.address);
+  //     console.log("   Estate Address     : ", estateAddr);
+  //     await space.assignMultipleRoods([3], [3], creator, sentByCreator);
+  //     estateId = await createEstate([3], [3], user, sentByCreator);
 
-      console.log("   Estate token ID    : ", estateId);
-    });
+  //     console.log("   Estate token ID    : ", estateId);
+  //   });
 
-    describe("transferSpaceToEstate", function () {
-      it("should not transfer the SPACE to an Estate if it is not owned by the sender", async function () {
-        await space.assignMultipleRoods([4], [4], operator, sentByCreator);
-        await assertRevert(
-          space.transferSpaceToEstate(4, 4, estateId, sentByOperator)
-        );
-      });
+  //   describe("transferSpaceToEstate", function () {
+  //     it("should not transfer the SPACE to an Estate if it is not owned by the sender", async function () {
+  //       await space.assignMultipleRoods([4], [4], operator, sentByCreator);
+  //       await assertRevert(
+  //         space.transferSpaceToEstate(4, 4, estateId, sentByOperator)
+  //       );
+  //     });
 
-      it("transfers SPACE to an Estate if it is called by owner", async function () {
-        await space.transferSpaceToEstate(0, 1, estateId, sentByUser);
+  //     it("transfers SPACE to an Estate if it is called by owner", async function () {
+  //       await space.transferSpaceToEstate(0, 1, estateId, sentByUser);
 
-        let result = await space.spaceOf(estate.address);
-        const xEstate = result["0"];
-        const yEstate = result["1"];
+  //       let result = await space.spaceOf(estate.address);
+  //       const xEstate = result["0"];
+  //       const yEstate = result["1"];
 
-        result = await space.spaceOf(user);
-        const xNewUser = result["0"];
-        const yNewUser = result["1"];
+  //       result = await space.spaceOf(user);
+  //       const xNewUser = result["0"];
+  //       const yNewUser = result["1"];
 
-        web3.utils.fromWei(xEstate[0], "wei").should.be.equal("3");
-        web3.utils.fromWei(xEstate[1], "wei").should.be.equal("0");
-        web3.utils.fromWei(yEstate[0], "wei").should.be.equal("3");
-        web3.utils.fromWei(yEstate[1], "wei").should.be.equal("1");
+  //       web3.utils.fromWei(xEstate[0], "wei").should.be.equal("3");
+  //       web3.utils.fromWei(xEstate[1], "wei").should.be.equal("0");
+  //       web3.utils.fromWei(yEstate[0], "wei").should.be.equal("3");
+  //       web3.utils.fromWei(yEstate[1], "wei").should.be.equal("1");
 
-        xEstate.length.should.be.equal(2);
-        yEstate.length.should.be.equal(2);
+  //       xEstate.length.should.be.equal(2);
+  //       yEstate.length.should.be.equal(2);
 
-        web3.utils.fromWei(xNewUser[0], "wei").should.be.equal("0");
-        web3.utils.fromWei(yNewUser[0], "wei").should.be.equal("2");
+  //       web3.utils.fromWei(xNewUser[0], "wei").should.be.equal("0");
+  //       web3.utils.fromWei(yNewUser[0], "wei").should.be.equal("2");
 
-        xNewUser.length.should.be.equal(1);
-        yNewUser.length.should.be.equal(1);
-      });
-    });
-  });
+  //       xNewUser.length.should.be.equal(1);
+  //       yNewUser.length.should.be.equal(1);
+  //     });
+  //   });
+  // });
 });
